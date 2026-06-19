8412680e7f6ef7ff344702495dd3d76d2f34d155
joinovadigitale <123222179+jdavidg1483@users.noreply.github.com>
2026-06-19 11:45:15 -0500

    Se agrego el blog dinamico y sus categorias a la web

--- src/types/index.ts
+++ src/types/index.ts
@@ -45,8 +45,26 @@ export const BaseWPSchema = z.object({
     imagen: z.string()
   })
 
+  const CategorySchema = z.object({
+     name: z.string(),
+     slug: z.string()
+  })
+
+  const CategoriesSchema = z.array(CategorySchema)
+
   export const ProcessPageSchema = BaseWPSchema.extend({
     acf: z.object({
       subtitle: z.string()
     }).catchall(prcessSchema)
   })
+
+  export const PostSchema =  BaseWPSchema.omit({
+    acf:true
+  }).extend({
+    date: z.string(),
+    category_details: CategoriesSchema
+  })
+
+  export const PostsSchema = z.array(PostSchema)
+
+  export type Post = z.infer<typeof PostSchema>
